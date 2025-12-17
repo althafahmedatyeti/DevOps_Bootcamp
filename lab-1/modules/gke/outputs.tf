03c42a3e77f88f5fce38d60b7066a9faf16ac43e
Althaf Ahmed Md <althaf.ahmed@atyeti.com>
2025-12-17 12:05:58 +0530

    Update outputs.tf

--- lab-1/modules/gke/outputs.tf
+++ lab-1/modules/gke/outputs.tf
@@ -1,21 +1,22 @@
 output "cluster_name" {
-  value = google_container_cluster.autopilot.name
+  value = try(google_container_cluster.autopilot.name)
 }
 
 output "endpoint" {
-  value = google_container_cluster.autopilot.endpoint
+  value = try(google_container_cluster.autopilot.endpoint)
 }
 
 output "location" {
-  value = google_container_cluster.autopilot.location
+  value = try(google_container_cluster.autopilot.location)
 }
 
 output "cluster_id" {
-  value = google_container_cluster.autopilot.id
+  value = try(google_container_cluster.autopilot.id)
 }
 
 output "ca_certificate" {
-  value = google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate
+  value = try(google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate)
 }
+
 
 
